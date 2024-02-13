ba542131005ce648ae99e178f75e8c5eb73c24d9
Eunice Kim <112116257+eunykim92@users.noreply.github.com>
2024-02-12 19:08:54 -0800

    Need to PR

--- server/controllers/passportController.js
+++ server/controllers/passportController.js
@@ -47,10 +47,22 @@ function(accessToken, refreshToken, expiresIn, profile, done) {
     });
 });
 
+// serialize user into the session
+// serialization is the process of converting a user object into a format that can be stored in a session, it determines which data of the user object should be stored in the session
+// what happens doing serialization - 1. user logs in, passport will serialize the user instance with the information provided by the authentication strategy's verify callback
+// 2. passport will then save this serialized information, to the session
+// 3. session data is then sent to the client as a cookie
+// tldr, only the user ID is serialized to the session, so the session will be small and lightweight
 const serializeUser = (user, done) => {
     done(null, user.UserID);
 };
 
+// deserialize user from the session
+// opposite process of serialization. converts the serialized user information back into a user object
+// 1. on each request, passport will read the user ID from the session data in the session cookie that comes with the request
+// 2. passport will then use this ID to retrieve the full user object, typically from the database
+// 3. once the user object is retrieved, it's made available on 'req.user' in route handlers and middleware
+// tldr, when a quest is made, this function receives the ID from the session and uses it to retrieve the full user object from the database
 const deserializeUser = (id, done) => {
     db.query('SELECT * FROM Users WHERE UserID = $1', [id], (err, result) => {
         if (err) return done(err);
